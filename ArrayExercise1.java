6f4648cf34fbed106bddb7a870de4c4b6053cd2e
Lucky-kor <jungmin2056@gmail.com>
2020-12-07 18:06:12 +0900

    This is an example of finding the sum and average of an array using overlapped loops.

---
+++ ArrayExercise1.java
@@ -0,0 +1,31 @@
+public class ArrayExercise1 {
+    public static void main(String[] args) {
+
+        int[][] array = {
+        {95, 86},
+        {83, 92, 96},
+        {78, 83, 93, 87, 88}
+        };
+
+    int sum = 0;
+    double avg = 0.0;
+
+    //write your code
+
+    int count = 0;
+
+    for (int i=0; i<array.length; i++){
+        for(int j=0; j<array[i].length; j++){
+            sum += array[i][j];
+            count++;
+        }
+    }
+
+    avg = (double) sum / count;
+
+    //write your code
+
+        System.out.println("sum : " + sum);
+        System.out.println("avg : " + avg);
+    }
+}
