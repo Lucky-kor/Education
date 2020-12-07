0abe431abd9cd60781e9ea8151a621e0a0964ac9
Lucky-kor <jungmin2056@gmail.com>
2020-12-07 17:26:56 +0900

    This example uses an array to get the number of students and their scores to find the sum and average.

--- ArrayCreateByValueListExample1.java
+++ ArrayCreateByValueListExample1.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class ArrayCreateByValueListExample1 {
     public static void main(String[] args) {
-        int[]
+        int count;
+        int[] scores;
+        int sum = 0;
+        double avg;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("몇명의 점수를 입력할까요?");
+        count = scan.nextInt();
+        scores = new int[count];
+
+        for(int i = 0; i<count; i++){
+            System.out.println((i+1)+" 의 점수를 입력해 주세요");
+            scores[i]=scan.nextInt();
+        }
+        for(int j=0; j<scores.length; j++){
+            System.out.println((j+1)+" 의 점수는 : "+scores[j]);
+        }
+        for(int k=0; k<scores.length; k++){
+            sum += scores[k];
+        }
+        avg = (double) sum / scores.length;
+        System.out.println("모든 점수의 총합은 "+sum+" 입니다.");
+        System.out.println("점수의 평균값은 "+avg+" 입니다.");
     }
 }
