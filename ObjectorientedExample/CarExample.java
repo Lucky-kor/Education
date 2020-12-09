d7f792747d0b330738b3a7509d26d84af6bf0215
Lucky-kor <jungmin2056@gmail.com>
2020-12-09 14:27:23 +0900

    Typo correction

--- ObjectorientedExample/CarExample.java
+++ ObjectorientedExample/CarExample.java
@@ -3,7 +3,7 @@ package sec03.exam03;
 public class CarExample{
     public static void main(String[] args) {
         Car car1 = new Car();
-        System.out.println("car1.Company : " = car1.company);
+        System.out.println("car1.Company : " + car1.company);
         System.out.println();
 
         Car car2 = new Car("Own Car");
